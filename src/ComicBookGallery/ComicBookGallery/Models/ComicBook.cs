8719e09ca9dad46856830a052496b1021116a8b2
jankalasnikov <48696974+jankalasnikov@users.noreply.github.com>
2019-10-21 19:05:27 +0200

    Added Series and repaired titles in ComicBook repo

--- src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -8,17 +8,27 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public Series Series { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
+
         public string DisplayText
         {
             get
             {
-                return SeriesTitle + " " + IssueNumber;
+                var series = Series;
+                if (series != null)
+                {
+                    return Series.Title + " #" + IssueNumber;
+
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
 
@@ -27,10 +37,17 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-")
-                    .ToLower() + "-" + IssueNumber + ".jpg";
+                var series = Series;
+                if (series != null)
+                {
+                    return series.Title.Replace(" ", "-").ToLower() +
+                        "-" + IssueNumber + ".jpg";
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
-
     }
 }
